c99275168d33e9a61f459bb8aad5aa670f30c882
Afomachinenye <Afomachukwuchinenyeonyekaba@gmail.com>
2024-09-25 15:04:10 +0100

    Refactored some components

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,4 +47,45 @@ authController.post("/register", async (req, res) => {
   }
 });
 
+// Login controller
+authController.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        error: "Email and password fields are required",
+      });
+    }
+
+    // Check if the user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Compare the password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { _id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res.json({
+      status: true,
+      message: "Login successful!",
+      token,
+    });
+  } catch (error) {
+    console.error("Error during user login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = authController;

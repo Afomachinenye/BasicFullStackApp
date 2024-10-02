90472e0fd0607fafc5ed239a74776d47bbae5ca2
Afomachinenye <Afomachukwuchinenyeonyekaba@gmail.com>
2024-10-02 11:10:46 +0100

    Refactored some components

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const dotenv = require("dotenv").config();
 const connectDb = require("./db/ConnectDB");
 const bodyParser = require("body-parser");
 const authController = require("./controllers/authController");
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/auth", authController);
 
